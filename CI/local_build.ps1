540e5ea2867d2aaeec101f65c46737a33dd26d37
Vladimir Sumarov <summeroff@gmail.com>
2025-08-13 00:00:30 +0200

    fix build script

--- CI/local_build.ps1
+++ CI/local_build.ps1
@@ -1,8 +1,8 @@
 # Clone obs-sl-browser repository
-git clone --recursive https://github.com/streamlabs/obs-sl-browser.git
+#git clone --recursive https://github.com/streamlabs/obs-sl-browser.git
 
 # Read the obs.ver file to get the branch name
-$branchName = Get-Content -Path ".\obs-sl-browser\obs.ver" -Raw
+$branchName = Get-Content -Path ".\obs.ver" -Raw
 
 # Clone obs-studio repository with the branch name
 git clone --recursive --branch $branchName https://github.com/obsproject/obs-studio.git
@@ -17,7 +17,7 @@ $addSubdirectoryLine = "add_subdirectory(obs-sl-browser)"
 Add-Content -Path $cmakeListsPath -Value $addSubdirectoryLine
 
 # Move obs-sl-browser folder into obs-studio\plugins
-Move-Item -Path "..\obs-sl-browser" -Destination ".\plugins\obs-sl-browser"
+Move-Item -Path "..\..\obs-sl-browser" -Destination ".\plugins\obs-sl-browser"
 
 # Build
 #.\CI\build-windows.ps1
