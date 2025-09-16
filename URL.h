adda46bffc323f7c4ec398fb7e84eb504f226f8d
unknown <stevensoftware52@gmail.com>
2025-09-16 18:38:29 -0400

    Rebase

--- URL.h
+++ URL.h
@@ -2,8 +2,14 @@
 
 namespace URL
 {
-	static const char *getPluginHttpUrl()
+	static std::string getPluginHttpUrl()
 	{
+		char buffer[MAX_PATH];
+		DWORD len = GetEnvironmentVariableA("SL_PLUGIN_DEFAULT_URL", buffer, MAX_PATH);
+
+		if (len > 0 && len < MAX_PATH)
+			return buffer; 
+
 		return "https://obs-plugin.streamlabs.com";
 	}
 };
