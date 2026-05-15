656c970b4c48b1cb1cfed6aea6b89a1557b84987
unknown <stevensoftware52@gmail.com>
2026-05-14 21:16:27 -0400

    Update pipeline.ps1

--- CI/pipeline.ps1
+++ CI/pipeline.ps1
@@ -44,7 +44,15 @@ if ($LASTEXITCODE -ne 0) {
 }
 
 # Begin
-$env:Protobuf_DIR = "${github_workspace}\..\grpc_dist\cmake"
+if ($architecture -eq 'arm64') 
+{
+    $env:Protobuf_DIR = "${github_workspace}\..\grpc_dist\lib\cmake\protobuf"
+} 
+else 
+{
+    $env:Protobuf_DIR = "${github_workspace}\..\grpc_dist\cmake"
+}
+
 $env:absl_DIR = "${github_workspace}\..\grpc_dist\lib\cmake\absl"
 $env:gRPC_DIR = "${github_workspace}\..\grpc_dist\lib\cmake\grpc"
 $env:utf8_range_DIR = "${github_workspace}\..\grpc_dist\lib\cmake\utf8_range"
